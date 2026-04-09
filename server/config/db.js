c42564222bfc0299e556eb8143818bf7902c8322
0zzyM <ozzymacar@gmail.com>
2026-04-09 22:13:56 +0200

    feat(config): add MongoDB connection logic

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,14 @@
-  
+import mongoose from "mongoose";
+//import dotenv from "dotenv"; //not needed here as it is called in server.js
+
+export const connectDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connection Successful");
+  } catch (error) {
+    console.error("Connection to Atlas Failed", error);
+    process.exit(1);
+  }
+};
