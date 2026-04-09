834aac83aaa40572dd4daf515bfd661a58ced5e3
0zzyM <ozzymacar@gmail.com>
2026-04-09 22:14:23 +0200

    feat(server): integrate database connection #3

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 // Apperantly this needs to be on top otherwise process.env will get undefined
 dotenv.config(); // remember this loads .env into process.env
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(
   cors({
     origin: "http://localhost:5173", // only allow React dev server to send req
   }),
 );
+
 app.get("/", (req, res) => {
   res.send("Backend is running!!!");
 });
