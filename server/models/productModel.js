884b50d2e60c8aac07647cdbdf1c0ab954f95363
0zzyM <ozzymacar@gmail.com>
2026-04-10 14:08:02 +0200

    feat(models): created products model schema

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,1 +1,68 @@
-  
+import mongoose from "mongoose";
+
+// Create the Product schema
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ["guitars", "amps", "cabinets", "strings", "picks"],
+      required: true,
+    },
+    amountSold: {
+      type: Number,
+      default: 0,
+    },
+    reviewCount: {
+      type: Number,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    isOnSale: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    discountAmount: {
+      type: Number,
+      default: 0,
+    },
+    // TODO: Replace Mixed with category-specific schemas once all product types are defined
+
+    details: {
+      type: mongoose.Schema.Types.Mixed,
+      default: {},
+    },
+  },
+  { timestamps: true },
+);
+
+// Create the User model from the schema
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
