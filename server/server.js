bae5b40fd8602e21ffe9f5552377d308548c00a0
0zzyM <ozzymacar@gmail.com>
2026-04-11 17:31:13 +0200

    feat(server): mount product router and add json middleware #6 #7

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRouter from "./routers/productRouter.js";
 
 // Apperantly this needs to be on top otherwise process.env will get undefined
 dotenv.config(); // remember this loads .env into process.env
@@ -15,6 +16,10 @@ app.use(
     origin: "http://localhost:5173", // only allow React dev server to send req
   }),
 );
+
+app.use(express.json());
+
+app.use("/api/v1/products", productRouter);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!!!");
