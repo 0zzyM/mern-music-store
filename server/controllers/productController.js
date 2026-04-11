8d140e86adda0f5f31c2a559b5cbbfe37b7bb0b5
0zzyM <ozzymacar@gmail.com>
2026-04-11 15:17:54 +0200

    feat(controllers): add product controller with GET endpoints #6 #7

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,1 +1,8 @@
-  
+import Product from "../models/productModel.js";
+
+export const getAllProducts = async (req, res) => {
+  res.json({ message: "getAllProducts is working" });
+};
+export const getFeaturedProducts = async (req, res) => {};
+export const getProductsByCategory = async (req, res) => {};
+export const getFeaturedProductsByCategory = async (req, res) => {};
