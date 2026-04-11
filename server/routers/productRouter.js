532988886f3906d1d8d2512bba33372886194dd3
0zzyM <ozzymacar@gmail.com>
2026-04-11 17:30:21 +0200

    feat(routers): add product router with GET endpoints #6 #7

--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -1,1 +1,16 @@
-  
+import express from "express";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+  getProductsByCategory,
+  getFeaturedProductsByCategory,
+} from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/", getAllProducts);
+productRouter.get("/featured", getFeaturedProducts);
+productRouter.get("/:category", getProductsByCategory);
+productRouter.get("/:category/featured", getFeaturedProductsByCategory);
+
+export default productRouter;
