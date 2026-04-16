92c339d3f1560bc235c73251bb38d85638d70115
0zzyM <ozzymacar@gmail.com>
2026-04-16 10:29:36 +0200

    Revert "refactor(navbar): migrate to Tailwind CSS and remove CSS file #16"
    This reverts commit bd63368a5589f534930858adbd4d2c9c80f77263.

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -2,42 +2,51 @@ import cartIcon from "./assets/cart.svg";
 import userIcon from "./assets/user.svg";
 import searchIcon from "./assets/search.svg";
 
-const navMenuItem = "text-white hover:text-red-500 transition-colors";
-
+import "./Navbar.css";
 function Navbar() {
   return (
     <>
-      {/* Page Header */}
-      <header className="p-2.5 bg-[#001822] flex items-center justify-between w-full gap-10">
-        {/* Navigation Links */}
-        <nav className="w-60 flex justify-between items-center gap-10">
-          <a className={navMenuItem} href="#">
+      <header className="page-header">
+        <nav className="nav-wrapper">
+          <a className="nav-menu-item" href="#">
             Shop
           </a>
-          <a className={navMenuItem} href="#">
+          <a className="nav-menu-item" href="#">
             Services
           </a>
-          <a className={navMenuItem} href="#">
+          <a className="nav-menu-item" href="#">
             Contact
           </a>
         </nav>
 
-        {/* Logo */}
-        <div className="text-white text-2xl font-bold">ozzyMusic</div>
+        <div className="logo">ozzyMusic</div>
 
-        {/* Header Actions */}
-        <div className="w-60 flex justify-center items-center gap-5">
-          <a href="#">
-            <img className="w-9" src={searchIcon} alt="Search" />
-          </a>
-          <a href="#">
-            <img className="w-9" src={cartIcon} alt="Cart" />
-          </a>
-          <a href="#">
-            <img className="w-9" src={userIcon} alt="User" />
-          </a>
+        <div className="header-actions">
+          <div className="header-action">
+            <a className="header-action-item" href="#">
+              <img className="search-icon" src={searchIcon} alt="" />
+            </a>
+          </div>
+          <div className="login-register">
+            <a className="login-register-btn" href="#">
+              <img className="cart-icon" src={cartIcon} alt="" />
+            </a>
+          </div>
+          <div className="cart">
+            <a className="cart-btn" href="#">
+              <img className="user-icon" src={userIcon} alt="" />
+            </a>
+          </div>
         </div>
       </header>
+
+      {/* 
+      <div>
+        <div className="search-bar-wrapper">
+          <input className="search-bar" placeholder="Search" type="text" />
+        </div>
+      </div>
+    */}
     </>
   );
 }
