27552d0beddcc1ff18795b0742d2737eb96bbf32
0zzyM <ozzymacar@gmail.com>
2026-04-21 09:56:00 +0200

    refactor(app): clean up imports and update component paths #17

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import PromotionBanner from "./components/PromotionBanner/PromotionBanner.jsx";
 import FeaturedProducts from "./components/FeaturedProducts/FeaturedProducts.jsx";
+import ProductCarousel from "./components/ProductCarousel/ProductCarousel.jsx";
 function App() {
   return (
     <>
       <Navbar />
-      <PromotionBanner className="banner" />
+      <PromotionBanner />
       <FeaturedProducts />
     </>
   );
