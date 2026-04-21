ef252fd8d8d659bf8d9b7fa5794f55cbcc2d1544
0zzyM <ozzymacar@gmail.com>
2026-04-21 23:07:19 +0200

    feat(controllers): add sort query parameter to getAllProducts #30

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,11 +1,30 @@
 import Product from "../models/productModel.js";
+const VALID_SORTS = ["mostSold", "newest", "rating"];
 
 export const getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    const { sort } = req.query; // Same as (const sort = req.query.sort)
+
+    if (sort && !VALID_SORTS.includes(sort)) {
+      return res.status(400).json({ message: "Invalid sort parameter" });
+    }
+
+    let products;
+
+    if (sort === "mostSold") {
+      products = await Product.find().sort({ amountSold: -1 });
+    } else if (sort === "newest") {
+      products = await Product.find().sort({ createdAt: -1 });
+    } else if (sort === "rating") {
+      products = await Product.find().sort({ rating: -1 });
+    } else {
+      products = await Product.find();
+    }
+
     if (!products || products.length === 0) {
       return res.status(404).json({ message: "No products found" });
     }
+
     res.status(200).json(products);
   } catch (error) {
     console.error(error);
