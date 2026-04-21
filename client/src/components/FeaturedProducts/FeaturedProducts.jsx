8277ff3ae89b4ef38acb580d776bd16cf3938b82
0zzyM <ozzymacar@gmail.com>
2026-04-21 09:55:27 +0200

    refactor(featured-products): clean up fetch logic and integrate carousel #18

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,55 +1,35 @@
 import "./FeaturedProducts.css";
 import { useState } from "react";
 import { useEffect } from "react";
+import ProductCarousel from "../ProductCarousel/ProductCarousel";
 
 export default function FeaturedProducts() {
   const [featuredProducts, setFeaturedProducts] = useState(null);
 
   useEffect(() => {
     const getFeaturedProducts = async () => {
+      const url = "http://localhost:5000/api/v1/products/featured";
       try {
-        const url = "http://localhost:5000/api/v1/products/featured";
         const res = await fetch(url);
         const data = await res.json();
         setFeaturedProducts(data);
-        console.log(data);
       } catch (error) {
-        const url = "http://localhost:5000/api/v1/products/featured";
         console.error(`Error fetching ${url} `, error);
       }
     };
     getFeaturedProducts();
   }, []);
+
   if (!featuredProducts) return <p>Loading...</p>;
 
   return (
     <>
-      <>
-        <div className="seperator"></div>
-
+      <div className="featured-wrapper">
         <h2>Featured Products</h2>
-
-        <div className="featured-wrapper">
-          <div className="featured-product-container">
-            {featuredProducts.map((product) => {
-              return (
-                <div className="featured-product">
-                  <div className="featured-product-image-container">
-                    <img
-                      className="featured-product-image "
-                      src={product.images[0]}
-                      alt=""
-                    />
-                  </div>
-                  <h3 className="featured-product-title">{product.name}</h3>
-                  <p className="featured-product-price">€{product.price}</p>
-                  <button className="add-to-cart-btn"> Add to Cart</button>
-                </div>
-              );
-            })}
-          </div>
+        <div className="featured-product-container">
+          <ProductCarousel products={featuredProducts} />
         </div>
-      </>
+      </div>
     </>
   );
 }
