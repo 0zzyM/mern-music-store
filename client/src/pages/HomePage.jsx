5601e9143f8fc02b235d4ca11b61c37364033992
0zzyM <ozzymacar@gmail.com>
2026-04-21 12:04:47 +0200

    feat(pages): add HomePage component #21

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import PromotionBanner from "../components/PromotionBanner/PromotionBanner.jsx";
+import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts.jsx";
+
+export default function HomePage() {
+  return (
+    <>
+      <PromotionBanner />
+      <FeaturedProducts />
+    </>
+  );
+}
