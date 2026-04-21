81d41bb3f6d55909e0f32c50ac6facea35b923a8
0zzyM <ozzymacar@gmail.com>
2026-04-21 23:07:07 +0200

    feat(homepage): add best sellers section to homepage #29

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import PromotionBanner from "../components/PromotionBanner/PromotionBanner.jsx";
 import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts.jsx";
+import BestSellerProducts from "../components/BestSellers/BestSellerProducts.jsx";
 
 export default function HomePage() {
   return (
     <>
       <PromotionBanner />
       <FeaturedProducts />
+      <BestSellerProducts />
     </>
   );
 }
