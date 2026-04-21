0b14ae75b47537db5e9c58e7f48d0e3fb176418f
0zzyM <ozzymacar@gmail.com>
2026-04-21 09:55:01 +0200

    feat(product-card): add product card component with styling #19

---
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,19 @@
+import "./ProductCard.css";
+
+export default function ProductCard({ product }) {
+  return (
+    <div className="product">
+      <div className="product-image-container">
+        <img
+          className="product-image"
+          src={product.images[0]}
+          alt={product.name}
+        />
+      </div>
+      <p className="product-brand">{product.brand}</p>
+      <p className="product-title">{product.name}</p>
+      <p className="product-price">€{product.price}</p>
+      <button className="add-to-cart-btn"> Add to Cart</button>
+    </div>
+  );
+}
