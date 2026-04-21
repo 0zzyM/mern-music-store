c6869004984f84d4568abdbdaebf1707aaa45896
0zzyM <ozzymacar@gmail.com>
2026-04-21 12:04:26 +0200

    feat(router): configure BrowserRouter in main.jsx #21

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,10 @@ import App from "./App.jsx";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/700.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 );
