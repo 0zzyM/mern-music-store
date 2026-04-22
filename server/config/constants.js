fbed78861cfcc9a510092ec85b7e384ee838f03d
0zzyM <ozzymacar@gmail.com>
2026-04-22 10:44:24 +0200

    feat(config): add constants.js with CATEGORY_MAP

---
+++ server/config/constants.js
@@ -0,0 +1,14 @@
+export const CATEGORY_MAP = {
+  guitars: ["electric-guitars", "bass-guitars", "acoustic-guitars"],
+  amplification: ["amp-heads", "combo-amps", "cabinets", "bass-amps"],
+  effects: [
+    "overdrive-distortion",
+    "delay",
+    "reverb",
+    "modulation",
+    "multi-effects",
+  ],
+  accessories: ["guitar-strings", "bass-strings", "picks", "straps", "tuners"],
+  recording: ["audio-interfaces", "studio-headphones", "microphones"],
+  maintenance: ["guitar-tools", "guitar-care"],
+};
