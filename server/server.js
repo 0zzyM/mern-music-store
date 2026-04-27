4a07a97fdc99a519236c24a8c0812bc32f6412ba
0zzyM <ozzymacar@gmail.com>
2026-04-27 22:04:09 +0200

    feat(server): mount category and subcategory routers #36

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routers/productRouter.js";
+import subCategoryRouter from "./routers/subCategoryRouter.js";
+import categoryRouter from "./routers/categoryRouter.js";
 
 // Apperantly this needs to be on top otherwise process.env will get undefined
 dotenv.config(); // remember this loads .env into process.env
@@ -20,6 +22,8 @@ app.use(
 app.use(express.json());
 
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/subcategories", subCategoryRouter);
+app.use("/api/v1/categories", categoryRouter);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!!!");
