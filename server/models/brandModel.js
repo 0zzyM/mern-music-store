b6603adc2353fe1551a5f1d343746b18b422bb3f
0zzyM <ozzymacar@gmail.com>
2026-04-30 12:07:21 +0200

    feat(models): add brand model #37

--- server/models/brandModel.js
+++ server/models/brandModel.js
@@ -1,1 +1,33 @@
-  
+import mongoose from "mongoose";
+
+const brandSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+export default Brand;
