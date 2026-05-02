6c784519d51dc0d2d87dc03393f6459c962fa985
0zzyM <ozzymacar@gmail.com>
2026-05-02 13:03:39 +0200

    refactor(home): mount categories grid #28

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import PromotionBanner from "../components/PromotionBanner/PromotionBanner.jsx";
 import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts.jsx";
 import BestSellerProducts from "../components/BestSellers/BestSellerProducts.jsx";
+import CategoriesGrid from "../components/Categories/CategoriesGrid.jsx";
 
 export default function HomePage() {
   return (
     <>
       <PromotionBanner />
+      <CategoriesGrid />
       <FeaturedProducts />
       <BestSellerProducts />
     </>
