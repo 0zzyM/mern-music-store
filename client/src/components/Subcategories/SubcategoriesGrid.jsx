9971537d8cd85301a83d1fc3ea8d698439e7e8b8
0zzyM <ozzymacar@gmail.com>
2026-05-08 11:06:05 +0200

    feat(subcategories): add subcategories grid component #51

---
+++ client/src/components/Subcategories/SubcategoriesGrid.jsx
@@ -0,0 +1,33 @@
+import SubcategoryCard from "./SubcategoryCard";
+import "./Subcategories.css";
+
+import { useState, useEffect } from "react";
+
+export default function SubcategoriesGrid({ category }) {
+  const [subcategories, setSubcategories] = useState(null);
+
+  useEffect(() => {
+    const getSubcategories = async () => {
+      const url = `http://localhost:5000/api/v1/categories/${category}/subcategories`;
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setSubcategories(data);
+      } catch (error) {
+        console.error(`Error fetching ${url} `, error);
+      }
+    };
+    getSubcategories();
+  }, [category]);
+
+  if (!subcategories) return <p>Loading...</p>;
+  return (
+    <div className="subcategories-container">
+      {subcategories.map((subcategory) => {
+        return (
+          <SubcategoryCard key={subcategory._id} subcategory={subcategory} />
+        );
+      })}
+    </div>
+  );
+}
---
+++ client/src/components/Subcategories/SubcategoryCard.jsx
@@ -0,0 +1,22 @@
+import "./Subcategories.css";
+import { Link } from "react-router-dom";
+
+export default function SubcategoryCard({ subcategory }) {
+  return (
+    <Link to={`/products?subcategory=${subcategory.slug}`} className="category">
+      <div className="subcategory">
+        <div className="subcategory-image-container">
+          <img
+            className="subcategory-image"
+            src={subcategory.image}
+            alt={subcategory.slug}
+          />
+        </div>
+        <div className="subcategory-actions">
+          <p className="subcategory-title">{subcategory.name}</p>
+          <button className="explore-subcategory-btn">Explore</button>
+        </div>
+      </div>
+    </Link>
+  );
+}
