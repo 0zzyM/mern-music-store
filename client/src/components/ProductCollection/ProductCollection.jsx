072076072db223802e9acf520d82f6d673138a05
0zzyM <ozzymacar@gmail.com>
2026-05-08 11:02:54 +0200

    refactor(components): replace separate carousels with reusable ProductCollection #50

---
+++ client/src/components/ProductCollection/ProductCollection.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import ProductCarousel from "../ProductCarousel/ProductCarousel";
+
+export default function ProductCollection({
+  category,
+  sortOption,
+  isFeatured,
+  title,
+}) {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      let url = "http://localhost:5000/api/v1/products";
+
+      let params = "";
+
+      if (sortOption) {
+        params += `&sort=${sortOption}`;
+      }
+      if (category) {
+        params += `&category=${category}`;
+      }
+      if (isFeatured) {
+        params += `&isFeatured=${isFeatured}`;
+      }
+
+      if (params) url += "?" + params.substring(1); // removes the 1st char of params not(0) it should be (1)
+
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(`Error fetching ${url} `, error);
+      }
+    };
+    getProducts();
+  }, [category, sortOption, isFeatured]); // slug and title is excluded so the comp doesnt re-render
+
+  if (!products) return <p>Loading...</p>;
+
+  return <ProductCarousel products={products} title={title} />;
+}
