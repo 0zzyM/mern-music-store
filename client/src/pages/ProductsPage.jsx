1dae0d68435753218f3acf8c3c83c291724c74be
0zzyM <ozzymacar@gmail.com>
2026-05-16 15:52:47 +0200

    feat(pages) : mount header to products page #60

--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -1,20 +1,48 @@
 import { useSearchParams } from "react-router-dom";
-import ProductList from "../components/ProductListing/ProductList.jsx";
+import { useState, useEffect } from "react";
+import ProductList from "../components/ProductList/ProductList.jsx";
+import ProductsHeader from "../components/ProductsHeader/ProductsHeader.jsx";
 
 export default function ProductsPage() {
   const [searchParams] = useSearchParams();
+  const [title, setTitle] = useState("All Products");
 
-  // Build filter string from URL params
-
-  console.log(searchParams);
-  console.log(searchParams.toString());
-
+  const subcategory = searchParams.get("subcategory");
+  const category = searchParams.get("category");
   const filters = searchParams.toString() ? `?${searchParams.toString()}` : "";
 
-  console.log(filters);
+  useEffect(() => {
+    const getTitle = async () => {
+      if (subcategory) {
+        try {
+          const res = await fetch(
+            `http://localhost:5000/api/v1/subcategories/${subcategory}`,
+          );
+          const data = await res.json();
+          setTitle(data.name);
+        } catch (error) {
+          console.error(error);
+        }
+      } else if (category) {
+        try {
+          const res = await fetch(
+            `http://localhost:5000/api/v1/categories/${category}`,
+          );
+          const data = await res.json();
+          setTitle(data.name);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        setTitle("All Products");
+      }
+    };
+    getTitle();
+  }, [subcategory, category]);
+
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="products-wrapper">
+      <ProductsHeader title={title} />
       <ProductList filters={filters} />
     </div>
   );
