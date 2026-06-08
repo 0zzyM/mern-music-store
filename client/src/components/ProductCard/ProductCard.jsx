42d3304f65f3759c30602e094f805f113bf297db
0zzyM <ozzymacar@gmail.com>
2026-06-08 08:06:11 +0200

    refactor(product-card): Restyle and refactor product card #78

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,62 @@
 import "./ProductCard.css";
+import { FaStar, FaCartPlus } from "react-icons/fa";
+import { CiHeart, CiShoppingCart } from "react-icons/ci";
+import { CgArrowsExchangeAlt } from "react-icons/cg";
+import { LuHeart } from "react-icons/lu";
+import { Link } from "react-router-dom";
+import { PiHeartFill } from "react-icons/pi";
+import { BsCart2 } from "react-icons/bs";
+import { MdStarRate } from "react-icons/md";
 
 export default function ProductCard({ product }) {
   return (
     <div className="product">
-      <div className="product-image-container">
-        <img
-          className="product-image"
-          src={product.images[0]}
-          alt={product.name}
-        />
+      <Link to={`/products/${product._id}`}>
+        <div className="product-image-container">
+          <img
+            className="product-image"
+            src={product.images[0]}
+            alt={product.name}
+          />
+          <button
+            className="add-wishlist-btn"
+            alt="Add to Wishlist"
+            onClick={(e) => {
+              e.preventDefault();
+              {
+                /*TODO: Add Wishlist Logic */
+              }
+            }}
+          >
+            <PiHeartFill className="add-wishlist-icon" />
+          </button>
+
+          <button
+            className="product-card-compare-btn"
+            onClick={(e) => {
+              e.preventDefault();
+              {
+                /*TODO: Add Comparison Logic */
+              }
+            }}
+          >
+            <CgArrowsExchangeAlt className="product-card-compare-icon" />
+          </button>
+        </div>
+      </Link>
+      <div className="product-card-details">
+        <p className="product-title">{product.name}</p>
+        <p className="product-price">€{product.price}</p>
+        <div className="product-card-rating-wrapper">
+          <MdStarRate className="product-card-rating-icon" />
+          <p className="product-card-rating">{product.rating}</p>
+        </div>
       </div>
-      <p className="product-brand">{product.brand.name}</p>
-      <p className="product-title">{product.name}</p>
-      <p className="product-price">€{product.price}</p>
-      <button className="add-to-cart-btn"> Add to Cart</button>
+
+      <button className="product-card-add-to-cart-btn">
+        <BsCart2 className="add-to-cart-icon" />
+        <p className="add-to-cart-text">Add to Cart</p>
+      </button>
     </div>
   );
 }
