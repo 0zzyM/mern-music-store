19f0aac059f6c919eb06cd41e4a756991ae87814
0zzyM <ozzymacar@gmail.com>
2026-06-08 15:10:49 +0200

    feat(footer): add page footer with routing #68

---
+++ client/src/components/Footer/PageFooter.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./PageFooter.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function PageFooter() {
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const url = "http://localhost:5000/api/v1/categories";
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setCategories(data);
+      } catch (error) {
+        console.error(`Error fetching ${url} `, error);
+      }
+    };
+
+    getCategories();
+  }, []);
+
+  return (
+    <footer className="footer-container">
+      <div className="footer-socials">
+        <a
+          href="https://github.com/0zzyM"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="footer-socials-icon" />
+        </a>
+        <a
+          href="https://mt.linkedin.com/in/ozzyacar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="footer-socials-icon" />
+        </a>
+      </div>
+
+      <div className="footer-main">
+        {categories && (
+          <div className="footer-categories-container">
+            {categories.map((category) => {
+              return (
+                <div key={category._id} className="footer-category">
+                  <Link
+                    to={`/categories/${category.slug}`}
+                    className="footer-category-item"
+                  >
+                    {category.name}
+                  </Link>
+
+                  {category.subcategories.map((subcat) => {
+                    return (
+                      <Link
+                        key={subcat._id}
+                        to={`/products?subcategory=${subcat.slug}`}
+                        className="footer-subcategory-item"
+                      >
+                        {subcat.name}
+                      </Link>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        )}
+        <div className="footer-info">
+          <div className="footer-info-column">
+            <h4>Help</h4>
+            <a href="#">Customer Service</a>
+            <a href="#">Contact Us</a>
+            <a href="#">FAQ</a>
+            <a href="#">Terms & Conditions</a>
+          </div>
+
+          <div className="footer-info-column">
+            <h4>Company</h4>
+            <a href="#">About Us</a>
+            <a href="#">Careers</a>
+            <a href="#">Blog</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>© 2026 ozzyMusic. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
