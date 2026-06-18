6d5af4c0ea545884a566a34acf1625cdc56c81fd
0zzyM <ozzymacar@gmail.com>
2026-06-18 02:53:03 +0200

    feat(product-page): re-build product info component #91 #23

--- client/src/components/ProductPage/ProductInfo.jsx
+++ client/src/components/ProductPage/ProductInfo.jsx
@@ -1,16 +1,59 @@
+import "./ProductInfo.css";
+import { useState } from "react";
+
 export default function ProductInfo({ product }) {
+  // Use for formating the field keys into user readable text
+  const formatKey = (key) =>
+    key.replace(/([A-Z])/g, " $1").replace(/^./, (s) => s.toUpperCase());
+
+  const [isDetailsOpen, setIsDetailsOpen] = useState(true);
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(true);
+
+  const details = product.details ?? {};
+  const hasDetails = Object.keys(details).length > 0;
+
   return (
-    <>
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      {Object.keys(product.details).map((field) => {
-        return (
-          <>
-            <p>{field}</p>
-            <p>{product.details[field]}</p>
-          </>
-        );
-      })}
-    </>
+    <div className="product-info-wrapper">
+      {product.description && (
+        <>
+          <div
+            className="product-desc-header"
+            onClick={() => setIsDescriptionOpen(!isDescriptionOpen)}
+          >
+            <h4>Description</h4>
+            <span>{isDescriptionOpen ? "▼" : "▲"}</span>
+          </div>
+
+          {isDescriptionOpen && (
+            <p className="product-description">{product.description}</p>
+          )}
+        </>
+      )}
+      {hasDetails && (
+        <>
+          <div
+            className="product-specs-header"
+            onClick={() => setIsDetailsOpen(!isDetailsOpen)}
+          >
+            <h4>Specifications</h4>
+            <span>{isDetailsOpen ? "▼" : "▲"}</span>
+          </div>
+          {isDetailsOpen && (
+            <div className="product-specs">
+              {Object.keys(product.details).map((field) => {
+                return (
+                  <div className="product-spec" key={field}>
+                    <p className="product-spec-name">{formatKey(field)}</p>
+                    <p className="product-spec-value">
+                      {product.details[field]}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </>
+      )}
+    </div>
   );
 }
