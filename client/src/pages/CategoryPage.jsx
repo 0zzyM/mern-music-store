d6e306f30f360820c2823e8b0903b949c8eb7994
0zzyM <ozzymacar@gmail.com>
2026-06-19 00:43:43 +0200

    feat(category): add subcategory carousels #86 #95

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,28 +1,36 @@
 import { useParams } from "react-router-dom";
 import SubcategoriesGrid from "../components/Subcategories/SubcategoriesGrid.jsx";
 import ProductCollection from "../components/ProductCollection/ProductCollection.jsx";
+import { useEffect, useState } from "react";
 
 export default function CategoryPage() {
   const { category } = useParams();
 
+  const [subcategories, setSubcategories] = useState(null);
+
+  useEffect(() => {
+    const getSubcategories = async () => {
+      const res = await fetch(
+        `http://localhost:5000/api/v1/categories/${category}/subcategories`,
+      );
+      const data = await res.json();
+      setSubcategories(data);
+    };
+    getSubcategories();
+  }, [category]);
+
+  if (!subcategories) return <p>Loading...</p>;
+
   return (
     <>
       <SubcategoriesGrid category={category} />
-      <ProductCollection
-        category={category}
-        isFeatured={true}
-        title="Featured Products"
-      />
-      <ProductCollection
-        category={category}
-        title="Best Sellers"
-        sortOption="mostSold"
-      />
-      <ProductCollection
-        category={category}
-        title="New Arrivals"
-        sortOption="newest"
-      />
+      {subcategories.map((sub) => (
+        <ProductCollection
+          key={sub._id}
+          subcategory={sub.slug}
+          title={`Featured ${sub.name}`}
+        />
+      ))}
     </>
   );
 }
