0f9ee58d68d906a8d94af7f45bc5a9bf6e312cbe
0zzyM <ozzymacar@gmail.com>
2026-06-19 21:29:04 +0200

    chore(home): replace hardcoded values with config valiues #99

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import BrandCarousel from "../components/Brands/BrandCarousel.jsx";
 import "../App.css";
 import { useContext } from "react";
 import { SearchContext } from "../contexts/SearchContext.js";
+import { ITEMS_PER_PAGE } from "../config.js";
 
 export default function HomePage() {
   const { isSearching } = useContext(SearchContext);
@@ -19,15 +20,19 @@ export default function HomePage() {
       <ProductCollection
         isFeatured={true} // should enclose the boolean value in {}
         title="Featured Products"
-        limit="12"
+        limit={ITEMS_PER_PAGE}
       />
       <ProductCollection
         title="Best Sellers"
         sortOption="mostSold"
-        limit="12"
+        limit={ITEMS_PER_PAGE}
       />
 
-      <ProductCollection title="New Arrivals" sortOption="newest" limit="12" />
+      <ProductCollection
+        title="New Arrivals"
+        sortOption="newest"
+        limit={ITEMS_PER_PAGE}
+      />
 
       <BrandCarousel />
     </div>
