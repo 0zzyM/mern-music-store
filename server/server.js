226b9f78d07e97ca5269fa9eb870a715e70b1abd
0zzyM <ozzymacar@gmail.com>
2026-06-19 21:33:24 +0200

    chore(server): Change CORS origin to use CLIENT_URL env variable in server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // only allow React dev server to send req
+    origin: process.env.CLIENT_URL || "http://localhost:5173", // only allow deployed FE server and dev server to send req
   }),
 );
 
