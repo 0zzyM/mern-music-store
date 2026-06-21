a352cd9b6d98d47f1634412927149d35cce3d763
0zzyM <ozzymacar@gmail.com>
2026-06-21 09:29:40 +0200

    refactor(app): add cart routing to react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import CategoriesPage from "./pages/CategoriesPage.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
 import ProductsPage from "./pages/ProductsPage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
 
 import PageFooter from "./components/Footer/PageFooter.jsx";
 import { SearchProvider } from "./contexts/SearchProvider.jsx";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/categories/:category" element={<CategoryPage />} />
         <Route path="/products" element={<ProductsPage />}></Route>
         <Route path="/products/:id" element={<ProductPage />}></Route>
+        <Route path="/cart" element={<CartPage />}></Route>
       </Routes>
       <PageFooter />
     </SearchProvider>
