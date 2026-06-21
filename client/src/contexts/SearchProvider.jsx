556109b94bd953ee50c1799e6d8450b8cf39e061
0zzyM <ozzymacar@gmail.com>
2026-06-21 19:07:04 +0200

    refactor(search): move suggestions state to SearchContext #118

--- client/src/contexts/SearchProvider.jsx
+++ client/src/contexts/SearchProvider.jsx
@@ -3,9 +3,12 @@ import { SearchContext } from "./SearchContext";
 
 export function SearchProvider({ children }) {
   const [isSearching, setIsSearching] = useState(false);
+  const [suggestions, setSuggestions] = useState(null);
 
   return (
-    <SearchContext.Provider value={{ isSearching, setIsSearching }}>
+    <SearchContext.Provider
+      value={{ isSearching, setIsSearching, suggestions, setSuggestions }}
+    >
       {children}
     </SearchContext.Provider>
   );
