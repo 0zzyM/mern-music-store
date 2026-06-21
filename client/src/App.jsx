f19e8615e54f80289c5a6585fe141eff844ed655
0zzyM <ozzymacar@gmail.com>
2026-06-21 18:57:47 +0200

    feat(app): add backdrop for cart overlay and search blur #115 #117

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,25 +6,53 @@ import CategoryPage from "./pages/CategoryPage.jsx";
 import ProductsPage from "./pages/ProductsPage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
-
 import PageFooter from "./components/Footer/PageFooter.jsx";
-import { SearchProvider } from "./contexts/SearchProvider.jsx";
-
 import { Route, Routes } from "react-router-dom";
+import CartSidebar from "./components/Cart/CartSidebar.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { closeCart } from "./features/cartSlice";
+import { useContext } from "react";
+import { SearchContext } from "./contexts/SearchContext.js";
+
 function App() {
+  const isCartView = useSelector((state) => state.cart.isCartOpen);
+
+  const dispatch = useDispatch();
+
+  const { isSearching, setIsSearching, setSuggestions } =
+    useContext(SearchContext);
+
   return (
-    <SearchProvider>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/categories" element={<CategoriesPage />}></Route>
-        <Route path="/categories/:category" element={<CategoryPage />} />
-        <Route path="/products" element={<ProductsPage />}></Route>
-        <Route path="/products/:id" element={<ProductPage />}></Route>
-        <Route path="/cart" element={<CartPage />}></Route>
-      </Routes>
+      <div className={isSearching ? "page-content searching" : "page-content"}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/categories" element={<CategoriesPage />}></Route>
+          <Route path="/categories/:category" element={<CategoryPage />} />
+          <Route path="/products" element={<ProductsPage />}></Route>
+          <Route path="/products/:id" element={<ProductPage />}></Route>
+          <Route path="/cart" element={<CartPage />}></Route>
+        </Routes>
+        <div
+          className={
+            isCartView
+              ? "page-backdrop-active"
+              : isSearching
+                ? "page-backdrop-searching"
+                : "page-backdrop"
+          }
+          onClick={() => {
+            dispatch(closeCart());
+            setIsSearching(false);
+            setSuggestions(null);
+          }}
+        ></div>
+      </div>
+      <CartSidebar />
+
       <PageFooter />
-    </SearchProvider>
+    </>
   );
 }
 
