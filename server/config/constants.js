93fc209ebb75ab3ec3ca568cf2ef94e3ad65e449
0zzyM <ozzymacar@gmail.com>
2026-06-26 00:25:48 +0200

    refactor(subcategories): add clasical guitar and studio monitors and remove tuners

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,14 +1,25 @@
 export const CATEGORY_MAP = {
-  guitars: ["electric-guitars", "bass-guitars", "acoustic-guitars"],
+  guitars: [
+    "electric-guitars",
+    "bass-guitars",
+    "acoustic-guitars",
+    "clasical-guitars",
+  ],
   amplification: ["amp-heads", "combo-amps", "cabinets", "bass-amps"],
   effects: [
     "overdrive-distortion",
     "delay",
     "reverb",
+    "wah",
     "modulation",
     "multi-effects",
   ],
-  accessories: ["guitar-strings", "bass-strings", "picks", "straps", "tuners"],
-  recording: ["audio-interfaces", "studio-headphones", "microphones"],
+  accessories: ["guitar-strings", "bass-strings", "picks", "straps"],
+  recording: [
+    "audio-interfaces",
+    "studio-headphones",
+    "studio-monitors",
+    "microphones",
+  ],
   maintenance: ["guitar-tools", "guitar-care"],
 };
