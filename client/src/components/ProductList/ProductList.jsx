02e3b2750bcd35294a510ea8b4d719079dc0e25f
0zzyM <ozzymacar@gmail.com>
2026-07-01 14:19:34 +0200

    feat(ProductList): implement pagination UI with page navigation #110

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -2,10 +2,23 @@ import { useEffect } from "react";
 import { useState } from "react";
 import ProductListCard from "./ProductListCard";
 import "./ProductList.css";
-import { SERVER_URL } from "../../config.js";
+import { SERVER_URL, PRODUCTS_PER_PAGE } from "../../config.js";
+import { useFilters } from "../../hooks/useFilters.js";
 
 export default function ProductList({ filters }) {
   const [products, setProducts] = useState(null);
+  const [total, setTotal] = useState(null);
+
+  const {
+    goToPage,
+    goToNextPage,
+    goToPrevPage,
+    goToFirstPage,
+    goToLastPage,
+    page,
+  } = useFilters();
+
+  const currentPage = Number(page) || 1;
 
   useEffect(() => {
     const getProducts = async () => {
@@ -15,7 +28,8 @@ export default function ProductList({ filters }) {
       try {
         const res = await fetch(url);
         const data = await res.json();
-        setProducts(data);
+        setProducts(data.products);
+        setTotal(data.total);
       } catch (error) {
         console.error(`Error fetching ${url}`, error);
       }
@@ -25,11 +39,77 @@ export default function ProductList({ filters }) {
 
   if (!products) return <p>Loading...</p>;
 
+  if (total === 0) return <p style={{ width: "960px" }}>No product Found...</p>;
+
+  const NO_OF_PAGES = Math.ceil(total / PRODUCTS_PER_PAGE);
+
   return (
-    <div className="product-list">
-      {products.map((product) => {
-        return <ProductListCard key={product._id} product={product} />;
-      })}
+    <div className="product-list-wrapper">
+      <div className="product-list">
+        {products.map((product) => {
+          return <ProductListCard key={product._id} product={product} />;
+        })}
+      </div>
+
+      <div className="pagination-btn-wrapper">
+        <button
+          className="pagination-btn"
+          onClick={() => {
+            currentPage != 1 && goToFirstPage();
+          }}
+        >
+          &lt; &lt;
+        </button>
+        <button
+          className="pagination-btn"
+          onClick={() => {
+            currentPage != 1 && goToPrevPage();
+          }}
+        >
+          &lt;
+        </button>
+
+        {currentPage > 1 && (
+          <button
+            className="pagination-btn"
+            onClick={() => {
+              goToPage(currentPage - 1);
+            }}
+          >
+            {currentPage - 1}
+          </button>
+        )}
+
+        <button className="pagination-btn active">{currentPage}</button>
+
+        {currentPage < NO_OF_PAGES && (
+          <button
+            className="pagination-btn"
+            onClick={() => {
+              goToPage(currentPage + 1);
+            }}
+          >
+            {currentPage + 1}
+          </button>
+        )}
+
+        <button
+          className="pagination-btn"
+          onClick={() => {
+            currentPage != NO_OF_PAGES && goToNextPage(NO_OF_PAGES);
+          }}
+        >
+          &gt;
+        </button>
+        <button
+          className="pagination-btn"
+          onClick={() => {
+            currentPage != NO_OF_PAGES && goToLastPage(NO_OF_PAGES);
+          }}
+        >
+          &gt; &gt;
+        </button>
+      </div>
     </div>
   );
 }
