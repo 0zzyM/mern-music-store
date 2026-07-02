4b5af8c689a87ef4977b136d497022bc75aa7794
0zzyM <ozzymacar@gmail.com>
2026-07-02 23:42:15 +0200

    chore(navbar): create a component for bottom navbar menu on desktop and move the hardcoded categories to an item in config file pre responsive design implementation #49

---
+++ client/src/components/Navbar/DesktopBottomNavbar.jsx
@@ -0,0 +1,35 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { CATEGORIES } from "../../config.js";
+
+export default function DesktopBottomNavbar({ isSearching }) {
+  return (
+    <nav
+      className="page-header-bottom"
+      style={isSearching ? { visibility: "hidden" } : { opacity: "1" }}
+    >
+      {/*TODO: Consider re-styling Navbar dropdown */}
+      <ul className="categories-nav">
+        {CATEGORIES.map((category) => {
+          return (
+            <li key={category.slug} className="nav-item">
+              <Link to="/categories/guitars">{category.name}</Link>
+              <div className="header-dropdown">
+                {category.subcategories.map((subcategory) => {
+                  return (
+                    <Link
+                      to={`/products?subcategory=${subcategory.slug}`}
+                      className="header-dropdown-item"
+                    >
+                      {subcategory.name}
+                    </Link>
+                  );
+                })}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
--- client/src/config.js
+++ client/src/config.js
@@ -6,3 +6,67 @@ export const ITEMS_PER_PAGE = 12;
 
 // Used in Products Page
 export const PRODUCTS_PER_PAGE = 15;
+
+// Hardcoded used in Navbars
+export const CATEGORIES = [
+  {
+    name: "Guitars",
+    slug: "guitars",
+    subcategories: [
+      { name: "Electric Guitars", slug: "electric-guitars" },
+      { name: "Bass Guitars", slug: "bass-guitars" },
+      { name: "Acoustic Guitars", slug: "acoustic-guitars" },
+      { name: "Clasical Guitars", slug: "clasical-guitars" },
+    ],
+  },
+  {
+    name: "Amplification",
+    slug: "amplification",
+    subcategories: [
+      { name: "Amp Heads", slug: "amp-heads" },
+      { name: "Cabinets", slug: "cabinets" },
+      { name: "Combo Amps", slug: "combo-amps" },
+      { name: "Bass Amps", slug: "bass-amps" },
+    ],
+  },
+  {
+    name: "Effects",
+    slug: "effects",
+    subcategories: [
+      { name: "Overdrive & Distortion", slug: "overdrive-distortion" },
+      { name: "Reverb", slug: "reverb" },
+      { name: "Delay", slug: "delay" },
+      { name: "Wah", slug: "wah" },
+      { name: "Modulation", slug: "modulation" },
+      { name: "Multi-effects", slug: "multi-effects" },
+    ],
+  },
+  {
+    name: "Recording",
+    slug: "recording",
+    subcategories: [
+      { name: "Audio Interfaces", slug: "audio-interfaces" },
+      { name: "Studio Headphones", slug: "studio-headphones" },
+      { name: "Studio Monitors", slug: "studio-monitors" },
+      { name: "Microphones", slug: "microphones" },
+    ],
+  },
+  {
+    name: "Maintenance",
+    slug: "maintenance",
+    subcategories: [
+      { name: "Guitar Care", slug: "guitar-care" },
+      { name: "Guitar Tools", slug: "Guitar Tools" },
+    ],
+  },
+  {
+    name: "Accessories",
+    slug: "accessories",
+    subcategories: [
+      { name: "Guitar Strings", slug: "guitar-strings" },
+      { name: "Bass Strings", slug: "bass-strings" },
+      { name: "Picks", slug: "picks" },
+      { name: "Straps", slug: "straps" },
+    ],
+  },
+];
