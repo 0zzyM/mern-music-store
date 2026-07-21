8c15e0153b439090fe586deb8dbf136286a12a4f
0zzyM <ozzymacar@gmail.com>
2026-07-21 17:52:51 +0200

    add BRANDS_SLUG and SORT_OPTIONS to constants

--- server/config/constants.ts
+++ server/config/constants.ts
@@ -22,9 +22,42 @@ export const CATEGORY_MAP = {
     "microphones",
   ],
   maintenance: ["guitar-tools", "guitar-care"],
-};
+} as const;
 
+export const BRAND_SLUGS = [
+  "fender",
+  "gibson",
+  "ibanez",
+  "jackson",
+  "esp",
+  "prs",
+  "schecter",
+  "epiphone",
+  "marshall",
+  "mesa-boogie",
+  "orange",
+  "evh",
+  "boss",
+  "electro-harmonix",
+  "strymon",
+  "focusrite",
+  "shure",
+  "elixir",
+  "ernie-ball",
+  "daddario",
+  "dunlop",
+] as const;
+
+export type Brand = keyof typeof BRAND_SLUGS;
 export type Category = keyof typeof CATEGORY_MAP;
 export type SubCategory = (typeof CATEGORY_MAP)[Category][number];
 
 export const DEFAULT_PAGE_LIMIT = 15;
+
+export const SORT_OPTIONS = {
+  newest: { createdAt: -1 },
+  mostSold: { amountSold: -1 },
+  rating: { rating: -1 },
+  priceAsc: { price: 1 },
+  priceDesc: { price: -1 },
+} as const;
