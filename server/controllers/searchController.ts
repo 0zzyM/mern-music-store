2b4b9e5ab81376fde7733552d1c7ea0a30c85128
0zzyM <ozzymacar@gmail.com>
2026-07-23 01:30:52 +0200

    refactor(search-controller): implement search query validation and flip to inclusion list #158 #174

--- server/controllers/searchController.ts
+++ server/controllers/searchController.ts
@@ -3,30 +3,24 @@ import Product from "../models/productModel.js";
 import Category from "../models/categoryModel.js";
 import Subcategory from "../models/subcategoryModel.js";
 import type { Request, Response } from "express";
+import type {
+  SearchQueryDTO,
+  SuggestQueryDTO,
+} from "../validation/searchQuerySpecs.js";
 
-const PRODUCT_SEARCH_FIELDS =
-  "-__v -createdAt -updatedAt -isActive -brand -category -subcategory -amountSold -reviewCount -rating -isFeatured";
+const PRODUCT_SEARCH_FIELDS = "name images price stock isOnSale discountAmount";
 
-const BRAND_SEARCH_FIELDS = "-__v -createdAt -updatedAt -isActive -description";
+const BRAND_SEARCH_FIELDS = "name slug image";
 
-const CATEGORY_SEARCH_FIELDS =
-  "-__v -createdAt -updatedAt -isActive -subcategories -description";
+const CATEGORY_SEARCH_FIELDS = "name slug image";
 
-const SUBCATEGORY_SEARCH_FIELDS =
-  "-__v -createdAt -updatedAt -isActive -parentCategory -description";
+const SUBCATEGORY_SEARCH_FIELDS = "name slug image";
 
 export const getAllSearchResults = async (req: Request, res: Response) => {
   try {
-    const { q } = req.query;
+    const dto = req.validatedQuery as SearchQueryDTO;
 
-    //q.trim().length === 0 in case just spaces etc.
-
-    if (typeof q !== "string" || q.trim().length === 0) {
-      return res.status(400).json({ message: "Search query is required" });
-    }
-
-    //Trim was needed otherwise + will be interpreted as a space by any HTTP parser
-    const safeQuery = q.replace(/[.*+?^${}()|[\]\\]/g, "\\$&").trim();
+    const safeQuery = dto.q.replace(/[.*+?^${}()|[\]\\]/g, "\\$&").trim();
 
     const regex = new RegExp(safeQuery, "i");
 
@@ -36,7 +30,9 @@ export const getAllSearchResults = async (req: Request, res: Response) => {
         isActive: true,
       },
       PRODUCT_SEARCH_FIELDS,
-    ).lean();
+    )
+      .limit(dto.limit)
+      .lean();
 
     //(!productResults) was removed as if none it  will be []
     if (productResults.length === 0) {
@@ -50,18 +46,15 @@ export const getAllSearchResults = async (req: Request, res: Response) => {
   }
 };
 
+//Limit is hardcoded here 5 2 2 3
 export const getSuggestedSearchResults = async (
   req: Request,
   res: Response,
 ) => {
   try {
-    const { q } = req.query;
+    const dto = req.validatedQuery as SuggestQueryDTO;
 
-    if (typeof q !== "string" || q.trim().length === 0) {
-      return res.status(400).json({ message: "Search query is required" });
-    }
-
-    const safeQuery = q.replace(/[.*+?^${}()|[\]\\]/g, "\\$&").trim();
+    const safeQuery = dto.q.replace(/[.*+?^${}()|[\]\\]/g, "\\$&").trim();
 
     const regex = new RegExp(safeQuery, "i");
 
