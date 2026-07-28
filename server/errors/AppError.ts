83ba65f8d02154addf8430e3614a09cb9653f9eb
0zzyM <ozzymacar@gmail.com>
2026-07-28 11:26:21 +0200

    add errorHandler middleware and AppError classes #98 #158

---
+++ server/errors/AppError.ts
@@ -0,0 +1,23 @@
+// abstract added so never throws app error directly only subclass
+export abstract class AppError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+  }
+}
+
+// Generic messages here are default, when throwing error will be passed detailed.
+
+export class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(message, 404);
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message = "Bad request") {
+    super(message, 400);
+  }
+}
---
+++ server/middlewares/errorHandler.ts
@@ -0,0 +1,13 @@
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors/AppError.js";
+
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction, //!this must be here (4 params) or Express won't treat as an error handler
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+};
