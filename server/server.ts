f6d1d2d0102f9590e3323a2c91ba4b0f9bd13e33
0zzyM <ozzymacar@gmail.com>
2026-07-29 14:44:53 +0200

    feat(rate-limit): mount rate limiting middlewares to server #158 #179

--- server/server.ts
+++ server/server.ts
@@ -9,6 +9,8 @@ import brandRouter from "./routers/brandRouter.js";
 import promotionRouter from "./routers/promotionRouter.js";
 import searchRouter from "./routers/searchRouter.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import { appLimiter, searchLimiter } from "./middlewares/rateLimiter.js";
+import { NotFoundError } from "./errors/AppError.js";
 
 // Only allowed here on ES modules bcs it is the top lvl no need for async fn
 await connectDB();
@@ -16,11 +18,21 @@ await connectDB();
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
+app.set("trust proxy", 1);
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173", // only allow deployed FE server and dev server to send req
   }),
 );
+
+//noUnusedParameters  doesn't allow to use req if I add _ before it will pass the test of it
+//Health Check
+app.get("/", (_req, res) => {
+  res.send("Backend is running!!!");
+});
+
+app.use(appLimiter);
 
 app.use(express.json());
 
@@ -29,17 +41,11 @@ app.use("/api/v1/subcategories", subCategoryRouter);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/brands", brandRouter);
 app.use("/api/v1/promotions", promotionRouter);
-app.use("/api/v1/search", searchRouter);
-
-//noUnusedParameters  doesn't allow to use req if I add _ before it will pass the test of it
-//Health Check
-app.get("/", (_req, res) => {
-  res.send("Backend is running!!!");
-});
+app.use("/api/v1/search", searchLimiter, searchRouter);
 
 // It is basically for all the paths  since no path specified as the first arg
 // Only the routes that don't exist will reach it
-app.use((_req, res) => res.status(404).json({ message: "Route not found" })); // unmatched routes
+app.use((_req, _res, next) => next(new NotFoundError("Route not found")));
 
 app.use(errorHandler);
 
