e07b46b7ebdc73fd0d1fee545dfd424c114d9827
Oguzhan <ozzymacar@gmail.com>
2026-08-02 13:40:12 +0200

    Revert "feat(useCategories): add custom hook for reactQuery implementation #103"
    This reverts commit 87ba8df94145a1c9f3a5cc9de06a4fa0598b3ba5.

--- client/src/api/categories.ts
+++
@@ -1,6 +0,0 @@
-import { fetchApi } from "./fetchApi";
-import { SERVER_URL } from "../config";
-import type { Category } from "../types/CategoryType";
-
-export const getCategories = () =>
-  fetchApi<Category[]>(`${SERVER_URL}/api/v1/categories`);
--- client/src/hooks/useCategories.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCategories } from "../api/categories";
-
-export const useCategories = () =>
-  useQuery({
-    queryKey: ["categories"],
-    queryFn: getCategories,
-    staleTime: Infinity,
-  });
