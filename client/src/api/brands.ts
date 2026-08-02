8e933d2c50c53a0bba0104b1e912f4ecb7b4822f
Oguzhan <ozzymacar@gmail.com>
2026-08-02 13:40:12 +0200

    Revert "feat(useBrands): create useBrands custom hook for implementation #103"
    This reverts commit c849a6001d84cd6dea03eb00b0f6cb5a90915cf9.

--- client/src/api/brands.ts
+++
@@ -1,6 +0,0 @@
-import { fetchApi } from "./fetchApi";
-import { SERVER_URL } from "../config";
-import type { Brand } from "../types/BrandType";
-
-export const getBrands = (limit: number) =>
-  fetchApi<Brand[]>(`${SERVER_URL}/api/v1/brands?limit=${limit}`);
