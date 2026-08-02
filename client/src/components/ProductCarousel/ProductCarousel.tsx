75c234521c46b69a38226bcd81534dca426ae9e9
0zzyM <ozzymacar@gmail.com>
2026-08-02 10:14:27 +0200

    chore(productCarousel): migrate to TS #188

--- client/src/components/ProductCarousel/ProductCarousel.tsx
+++ client/src/components/ProductCarousel/ProductCarousel.tsx
@@ -3,10 +3,19 @@ import "./ProductCarousel.css";
 import { Link } from "react-router-dom";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { useState } from "react";
+import type { Product } from "../../types/ProductType";
 const VISIBLE_CARDS = 6;
 const SLIDE_AMOUNT = 300;
 
-export default function ProductCarousel({ products, title, viewAllLink }) {
+export default function ProductCarousel({
+  products,
+  title,
+  viewAllLink,
+}: {
+  products: Product[];
+  title: string;
+  viewAllLink: string;
+}) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const isLastSlide = currentIndex >= products.length - VISIBLE_CARDS;
