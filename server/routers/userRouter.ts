a0c86867757c8ee7a9ad131cc12d2eb517dbb2a0
0zzyM <ozzymacar@gmail.com>
2026-08-16 14:32:22 +0200

    feat(userRouter): add router and  seed a  user #199

--- server/routers/userRouter.ts
+++ server/routers/userRouter.ts
@@ -1,1 +1,12 @@
-  
+import express from "express";
+import { getUser, getUsers } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+// To get all users should  be avoided until admin panel implemented irl
+// Use it for test and deactivate it
+userRouter.get("/", getUsers);
+
+userRouter.get("/:_id", getUser);
+
+export default userRouter;
