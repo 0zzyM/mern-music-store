38a63898965100a6e364f73415c9a943e97ef7fc
0zzyM <ozzymacar@gmail.com>
2026-08-21 12:25:38 +0200

    refactor(userService): add outbound inclusive fields and comment out the methods to not serve any data until JWT and admin auth

--- server/services/userService.ts
+++ server/services/userService.ts
@@ -2,21 +2,34 @@ import { isValidObjectId } from "mongoose";
 import { BadRequestError, NotFoundError } from "../errors/AppError.js";
 import User from "../models/userModel.js";
 
+/*
+const DEFAULT_FIELDS = "name surname email phoneNumber createdAt";
+*/
+const PROFILE_FIELDS = "name surname email phoneNumber address createdAt";
+
 export const getAllUsers = async () => {
-  const users = await User.find();
+  /*
+  const users = await User.find({}, DEFAULT_FIELDS);
 
   return users;
+  */
+
+  return null;
 };
 
 export const getUserById = async (id: string) => {
   if (!isValidObjectId(id)) {
     throw new BadRequestError("User ID is not valid.");
   }
-  const user = await User.findOne({ _id: id }).lean();
+  const user = await User.findOne({ _id: id }, PROFILE_FIELDS).lean();
 
   if (!user) {
     throw new NotFoundError("User was not found");
   }
 
+  return null;
+
+  /*
   return user;
+  */
 };
