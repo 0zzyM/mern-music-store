e9edd10d81a7e88e96edad9ffc17d6203a397b6d
0zzyM <ozzymacar@gmail.com>
2026-08-21 12:28:19 +0200

    feat(validation): add registrationBodySpecs for validation #204

---
+++ server/validation/registrationBodySpecs.ts
@@ -0,0 +1,38 @@
+import type { BodySpec } from "../middlewares/bodyHandler.js";
+const MIN_LENGTH = 2;
+const MAX_LENGTH = 128;
+
+export const registrationBodySpecs: BodySpec = {
+  name: {
+    type: "string",
+    minLength: MIN_LENGTH,
+    maxLength: MAX_LENGTH,
+    required: true,
+  },
+  surname: {
+    type: "string",
+    minLength: MIN_LENGTH,
+    maxLength: MAX_LENGTH,
+    required: true,
+  },
+  email: {
+    type: "email",
+    required: true,
+  },
+  phoneNumber: {
+    type: "phoneNumber",
+    required: true,
+  },
+  password: {
+    type: "password",
+    required: true,
+  },
+};
+
+export type registrationBodyDTO = {
+  name: string;
+  surname: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+};
