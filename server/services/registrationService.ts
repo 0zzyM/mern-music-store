73076ab32f803651e937dd65a950f0cd886b4276
0zzyM <ozzymacar@gmail.com>
2026-08-21 12:38:17 +0200

    fix(registrationService): consume validated input and catch duplicate key race conditions as 11000 errors and throw 409 already exist err #204

--- server/services/registrationService.ts
+++ server/services/registrationService.ts
@@ -1,8 +1,8 @@
 import { saltRounds } from "../config/constants.js";
 import { AlreadyExistsError } from "../errors/AppError.js";
 import User from "../models/userModel.js";
-import type { UserDoc } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import type { registrationBodyDTO } from "../validation/registrationBodySpecs.js";
 
 export const isUserMailExist = async (email: string) => {
   const user = await User.findOne({ email: email }).lean();
@@ -14,7 +14,7 @@ export const isPhoneNumberExist = async (phoneNumber: string) => {
   return user ? true : false;
 };
 
-export const handleRegistration = async (user: UserDoc) => {
+export const handleRegistration = async (user: registrationBodyDTO) => {
   if (await isUserMailExist(user.email)) {
     throw new AlreadyExistsError(
       `User with email:${user.email} already exists`,
@@ -27,28 +27,38 @@ export const handleRegistration = async (user: UserDoc) => {
     );
   }
 
-  console.log(user);
-
   const userPassword = user.password;
-
-  console.log(userPassword);
-
   const hashedPassword = await bcrypt.hash(userPassword, saltRounds);
-
-  console.log(hashedPassword);
 
   const secureUser = {
     ...user,
     password: hashedPassword,
   };
 
+  //! added cause here it was just the generic message without it not knowing if from mongoose, server, why etc.
   try {
     await User.create(secureUser);
     //TODO: More should be returned in the future for login redirect
+    //TODO: Just be careful here not to leak the bcrypt hash
     return { message: "Registration is successful" };
   } catch (error) {
-    throw new Error(
-      "Server Error:  Something went wrong, registration has failed",
-    );
+    //! Relies on auto-indexing at the moment may  require .init() in the future
+    //! 11000 is the err code for duplicate entry in DB!!!!
+    //! it is already validated one by one but if 2 very close request comes and reach to mongoose.write this is needed to understand
+    //! the race conditions happen so it's a good practice, i saw incidents where similar issues occured and it is a good practice to log it properly
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "code" in error &&
+      error.code === 11000
+    ) {
+      throw new AlreadyExistsError(
+        "A user with one of the detail already exist",
+      );
+    }
+
+    throw new Error("Something went wrong, registration has failed", {
+      cause: error,
+    });
   }
 };
