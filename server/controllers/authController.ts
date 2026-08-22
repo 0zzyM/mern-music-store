e6960dd0c30bea080b59177e1dc69435de0f9cef
0zzyM <ozzymacar@gmail.com>
2026-08-22 10:09:28 +0200

    feat(authController): wire loginService #209

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,9 +1,16 @@
 import type { Request, Response } from "express";
 import { handleRegistration } from "../services/registrationService.js";
 import type { registrationBodyDTO } from "../validation/registrationBodySpecs.js";
+import { handleLogin } from "../services/loginService.js";
 
 export const registerUser = async (req: Request, res: Response) => {
   const user = req.validatedBody as registrationBodyDTO;
   const response = await handleRegistration(user);
   res.status(201).json(response);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const user = req.body;
+  const response = await handleLogin(user);
+  res.status(200).json(response);
 };
