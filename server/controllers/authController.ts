7096222015a621881809487d183c7d633bc840f7
0zzyM <ozzymacar@gmail.com>
2026-08-22 11:39:48 +0200

    refactor(authController): consume validatedBody for login input #210

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,16 +1,17 @@
 import type { Request, Response } from "express";
 import { handleRegistration } from "../services/registrationService.js";
-import type { registrationBodyDTO } from "../validation/registrationBodySpecs.js";
+import type { RegistrationBodyDTO } from "../validation/registrationBodySpecs.js";
 import { handleLogin } from "../services/loginService.js";
+import type { LoginBodyDTO } from "../validation/loginBodySpecs.js";
 
 export const registerUser = async (req: Request, res: Response) => {
-  const user = req.validatedBody as registrationBodyDTO;
+  const user = req.validatedBody as RegistrationBodyDTO;
   const response = await handleRegistration(user);
   res.status(201).json(response);
 };
 
 export const login = async (req: Request, res: Response) => {
-  const user = req.body;
+  const user = req.validatedBody as LoginBodyDTO;
   const response = await handleLogin(user);
   res.status(200).json(response);
 };
