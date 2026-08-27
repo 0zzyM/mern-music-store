49be116df3ae9eb15c5022c23761ce1ce4fa5117
0zzyM <ozzymacar@gmail.com>
2026-08-27 23:51:36 +0200

    feat(models): add sessionModel, index expiresAt and userIds #213

---
+++ server/models/sessionModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }, //keep it will needed for logged in time
+);
+
+sessionSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+sessionSchema.index({ userId: 1 });
+
+const Session = mongoose.model("Session", sessionSchema);
+
+export type SessionDoc = mongoose.InferSchemaType<typeof sessionSchema>;
+
+export default Session;
