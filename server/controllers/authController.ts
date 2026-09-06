5ce990c7dd01e1e0361278582bf269997ff2fc4e
Oguzhan <ozzymacar@gmail.com>
2026-09-06 15:30:03 +0200

    feat(authController): return access and refreshToken as a httpOnly cookie using sameSite:strict and secure methods, refresh token path is set to /auth/refresh #216

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,6 +3,7 @@ import { handleRegistration } from "../services/registrationService.js";
 import type { RegistrationBodyDTO } from "../validation/registrationBodySpecs.js";
 import { handleLogin } from "../services/loginService.js";
 import type { LoginBodyDTO } from "../validation/loginBodySpecs.js";
+import { ACCESS_TOKEN_EXPIRES_MS } from "../config/constants.js";
 
 export const registerUser = async (req: Request, res: Response) => {
   const user = req.validatedBody as RegistrationBodyDTO;
@@ -12,12 +13,24 @@ export const registerUser = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
   const user = req.validatedBody as LoginBodyDTO;
-  const { accessToken, refreshToken } = await handleLogin(user);
 
-  console.log(accessToken, refreshToken);
+  const { accessToken, refreshToken, sessionExpiresAt } =
+    await handleLogin(user);
 
-  //TODO: decide if to use opaque refresh token or JWT
-  //TODO: decide if accessToken is stored in mem(redux) only or both as httpOnly cookie
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //TODO: later consider lax here again instead of strict
+    maxAge: ACCESS_TOKEN_EXPIRES_MS, //15 mins
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/auth/refresh",
+    expires: sessionExpiresAt,
+  });
 
   //Don't return anything yet!
   res.status(200).json("Login Successful");
