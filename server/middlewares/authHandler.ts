9e26091acdd16e7785a5bd18f286057b49c8f10c
0zzyM <ozzymacar@gmail.com>
2026-09-07 13:20:37 +0200

    refactor(authHandler): update it to use for validating tokens, old version was validating header, both tokens are stored as httpOnly cookie now

--- server/middlewares/authHandler.ts
+++ server/middlewares/authHandler.ts
@@ -1,28 +1,21 @@
 import { NextFunction, Request, Response } from "express";
-import { BadRequestError, UnauthorizedError } from "../errors/AppError.js";
-import jwt from "jsonwebtoken";
+import { UnauthorizedError } from "../errors/AppError.js";
+import { validateAccessToken } from "../services/tokenService.js";
+
+//TODO: Attach req.user later when ready
 
 export const validateToken = (
   req: Request,
   _res: Response,
   next: NextFunction,
 ) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) throw new BadRequestError("Missing Auth header!");
+  const accessToken = req.cookies.accessToken;
 
-  const secret = process.env.JWT_SECRET;
-  if (!secret) throw new Error("JWT_SECRET is not defined");
-
-  const [authType, token] = authHeader.split(" ");
-
-  if (authType !== "Bearer") throw new BadRequestError("Invalid Auth Header");
+  if (!accessToken) throw new UnauthorizedError("Missing Auth Token!");
 
   //TODO: fe needs to call refresh token endpoint if this fails.
-  try {
-    jwt.verify(token, secret);
-  } catch (error) {
-    throw new UnauthorizedError("Invalid token, login to continue.");
-  }
+  // validation will throw if access token is not valid
+  validateAccessToken(accessToken);
 
   next();
 };
